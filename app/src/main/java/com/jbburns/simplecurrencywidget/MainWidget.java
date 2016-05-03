02cc09a09b627ca0ea6d188a9ed0248cf7e702f3
Jonathan Burns <jbburns@users.noreply.github.com>
2016-05-03 13:31:49 +0900

    Added internet connection state checks for whenever a rate update is called

--- app/src/main/java/com/jbburns/simplecurrencywidget/MainWidget.java
+++ app/src/main/java/com/jbburns/simplecurrencywidget/MainWidget.java
@@ -5,6 +5,8 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.widget.RemoteViews;
 
 import org.apache.commons.lang.math.NumberUtils;
@@ -20,10 +22,18 @@ import java.util.Date;
 public class MainWidget extends AppWidgetProvider {
     public static String REFRESH_BUTTON_CLICKED = "com.jbburns.simplecurrencywidget.refreshButton_";
 
+    private static boolean isNetworkConnected (Context myContext){
+        ConnectivityManager cm =
+                (ConnectivityManager)myContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnectedOrConnecting();
+
+        return isConnected;
+    }
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.main_widget);
 
@@ -33,15 +43,15 @@ public class MainWidget extends AppWidgetProvider {
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
         views.setOnClickPendingIntent(R.id.refreshButton, pendingIntent);
 
-        String counterCurrency = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId,"counterCurrency");
-        String rateProvider = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId,"rateProvider");
-        String baseCurrency = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId,"baseCurrency");
-        String baseAmount  = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId, "baseAmount");
-        String feePercentage  = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId, "feePercentage");
-        String buySell  = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId, "buySell");
+        String counterCurrency = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId, "counterCurrency");
+        String rateProvider = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId, "rateProvider");
+        String baseCurrency = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId, "baseCurrency");
+        String baseAmount = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId, "baseAmount");
+        String feePercentage = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId, "feePercentage");
+        String buySell = MainWidgetConfigureActivity.loadStringPreference(context, appWidgetId, "buySell");
         String baseCounterCurrencyText = baseCurrency + "/" + counterCurrency;
-        if(NumberUtils.isNumber(baseAmount)){
-            if (Double.parseDouble(baseAmount) != 1){
+        if (NumberUtils.isNumber(baseAmount)) {
+            if (Double.parseDouble(baseAmount) != 1) {
                 if (buySell.equals("Sell")) {
                     baseCounterCurrencyText = baseCurrency + "/" + baseAmount + " " + counterCurrency;
                 } else {
@@ -54,50 +64,55 @@ public class MainWidget extends AppWidgetProvider {
         Date asOfTime = null;
         Double finalRate;
         Double rate = null;
-        if(rateProvider.equals(context.getResources().getString(R.string.smbctbRateProviderText))){
-            SMBCTBRate SMBCTBRate;
-            SMBCTBRateProvider smbctbRateProvider = new SMBCTBRateProvider();
-            SMBCTBRate = smbctbRateProvider.getRateByBaseCurrency(baseCurrency);
-            rate = SMBCTBRate.getMidRate();
-            asOfTime = SMBCTBRate.getAsOfTime();
-        }
 
-        // Calc the final rate
-        if(rate != null){
-            if (!NumberUtils.isNumber(feePercentage)){
-                feePercentage = "0";
-            }
-            if (buySell.equals("Sell")){
-                finalRate =  Double.parseDouble(baseAmount) / (rate + Double.parseDouble(feePercentage));
-            } else {
-                finalRate = (rate + Double.parseDouble(feePercentage)) * Double.parseDouble(baseAmount);
+        if(!isNetworkConnected(context)){
+            views.setTextColor(R.id.widgetRefreshTimeTextView, android.graphics.Color.RED);
+        }
+        else {
+            if (rateProvider.equals(context.getResources().getString(R.string.smbctbRateProviderText))) {
+                SMBCTBRate SMBCTBRate;
+                SMBCTBRateProvider smbctbRateProvider = new SMBCTBRateProvider();
+                SMBCTBRate = smbctbRateProvider.getRateByBaseCurrency(baseCurrency);
+                rate = SMBCTBRate.getMidRate();
+                asOfTime = SMBCTBRate.getAsOfTime();
             }
 
-            if (Double.parseDouble(feePercentage) > 0) {
-                finalRateString = String.format("%.2f", finalRate) + "*";
-            } else {
-                finalRateString = String.format("%.2f", finalRate);
+            // Calc the final rate
+            if (rate != null) {
+                if (!NumberUtils.isNumber(feePercentage)) {
+                    feePercentage = "0";
+                }
+                if (buySell.equals("Sell")) {
+                    finalRate = Double.parseDouble(baseAmount) / (rate + Double.parseDouble(feePercentage));
+                } else {
+                    finalRate = (rate + Double.parseDouble(feePercentage)) * Double.parseDouble(baseAmount);
+                }
+
+                if (Double.parseDouble(feePercentage) > 0) {
+                    finalRateString = String.format("%.2f", finalRate) + "*";
+                } else {
+                    finalRateString = String.format("%.2f", finalRate);
+                }
+
+                views.setTextViewText(R.id.rateDisplayTextView, finalRateString);
             }
 
-            views.setTextViewText(R.id.rateDisplayTextView,finalRateString);
+
+            //Set asoftime
+            if (asOfTime != null) {
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MMM-dd HH:mm");
+                views.setTextViewText(R.id.rateAsOfTimeTextView, "As of: " + df.format(asOfTime));
+            }
+
+            //Set update time
+            Calendar c = Calendar.getInstance();
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss");
+            String formattedDate = df.format(c.getTime());
+            views.setTextViewText(R.id.widgetRefreshTimeTextView, formattedDate);
+            views.setTextColor(R.id.widgetRefreshTimeTextView, android.graphics.Color.WHITE);
         }
-
-
-        //Set asoftime
-        if (asOfTime != null) {
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MMM-dd HH:mm");
-            views.setTextViewText(R.id.rateAsOfTimeTextView, "As of: " + df.format(asOfTime));
-        }
-
-        //Set update time
-        Calendar c = Calendar.getInstance();
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss");
-        String formattedDate = df.format(c.getTime());
-        views.setTextViewText(R.id.widgetRefreshTimeTextView, formattedDate);
-
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
-
     }
 
     @Override
